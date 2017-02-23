02616cc078b7d1d7b458460ce4e3741363e438de
Hugh Morrison <hmorri32@gmail.com>
2017-02-23 13:06:06 -0700

    install testing dependencies, add neccessary file structure

---
+++ test/test.js
@@ -0,0 +1,11 @@
+import { shallow, mount, render } from 'enzyme';
+import {expect, assert }          from 'chai';
+import React                      from 'react';
+require ('locus')
+
+describe('testing wif weathrly', () => {
+  it('should frickin work ', () => {
+    
+  })
+})
+
